01f5d384664f7a5b99b77cf6de6d96a5b8f9891e
SofiaAristy1491 <ingsofiaaristizabal.visar@gmail.com>
2025-03-25 16:04:47 -0500

    Ejercicio resuelto

---
+++ Ejercicios/Ejercicio_1_5y1_6.py
@@ -0,0 +1,41 @@
+'''
+Escriba un programa que realice la comprobación 
+de la división. Recuerde:  
+Divisor * Cociente + Residuo = Dividendo 
+Tome como entrada dos números, realice la 
+división entre ellos y entregue al usuario un texto 
+descriptivo con la comprobación de la división. 
+'''
+
+#Operador Comprobación de la División
+
+x=2
+y=6
+
+print(y/x)
+
+'''
+Para realizar la comprobación de la División es: 
+(Divisor * Cociente) + Residuo = Dividendo 
+'''
+#Resultado (cociente * x)+Residuo = Dividendo (y)
+
+
+'''Ejercicio 1.6'''
+
+'''Teniendo en cuenta el ejercicio anterior calcule el 
+residuo con el símbolo de módulo % y entregue la 
+comprobación con los valores resultantes de dividir 
+dos números entregados por el usuario del 
+programa. '''
+
+ #residuo
+print(6%2)
+
+print(2*3+0)
+
+'''
+La anterior instrucción muestra la prueba de la división. Al aplicar 6%2 encontramos 
+como respuesta 3, si aplicamos 3*2= 6 y 6 más 0 tenemos el resultado de 6 que 
+es la prueba de la división
+'''
--- Ejercicios/ejercicio_1_6.py
+++
@@ -1,7 +0,0 @@
-'''
-Teniendo en cuenta el ejercicio anterior calcule el 
-residuo con el símbolo de módulo % y entregue la 
-comprobación con los valores resultantes de dividir 
-dos números entregados por el usuario del 
-programa. 
-'''
